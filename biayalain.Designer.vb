bbeb0a64f7cb9ebdc43ca0271b3f976768196c4c
Holly Molly <58068843+inibapakmu@users.noreply.github.com>
2020-09-16 01:04:01 +0700

    Biaya Lain (Project Software Distribusi)

---
+++ biayalain.Designer.vb
@@ -0,0 +1,432 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class biayalain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.cmbthn2 = New System.Windows.Forms.ComboBox
+        Me.Label11 = New System.Windows.Forms.Label
+        Me.cmbbln2 = New System.Windows.Forms.ComboBox
+        Me.totalbalance = New System.Windows.Forms.TextBox
+        Me.txttotal = New System.Windows.Forms.TextBox
+        Me.Label7 = New System.Windows.Forms.Label
+        Me.Label8 = New System.Windows.Forms.Label
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button5 = New System.Windows.Forms.Button
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label10 = New System.Windows.Forms.Label
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.txtkodegl = New System.Windows.Forms.TextBox
+        Me.Label9 = New System.Windows.Forms.Label
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.txtjml = New System.Windows.Forms.TextBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.cmbjenis = New System.Windows.Forms.ComboBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.txtket = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.datagridstok = New System.Windows.Forms.DataGridView
+        Me.TextBox4 = New System.Windows.Forms.TextBox
+        Me.TextBox5 = New System.Windows.Forms.TextBox
+        Me.Label12 = New System.Windows.Forms.Label
+        Me.Button3 = New System.Windows.Forms.Button
+        CType(Me.datagridstok, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(455, 314)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(224, 17)
+        Me.TextBox3.TabIndex = 247
+        Me.TextBox3.Visible = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(488, 317)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(224, 17)
+        Me.TextBox2.TabIndex = 246
+        Me.TextBox2.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(431, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 245
+        Me.Button1.Text = "&Sort"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'cmbthn2
+        '
+        Me.cmbthn2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest
+        Me.cmbthn2.FormattingEnabled = True
+        Me.cmbthn2.Location = New System.Drawing.Point(295, 14)
+        Me.cmbthn2.Name = "cmbthn2"
+        Me.cmbthn2.Size = New System.Drawing.Size(121, 21)
+        Me.cmbthn2.TabIndex = 244
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(51, 21)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(34, 13)
+        Me.Label11.TabIndex = 243
+        Me.Label11.Text = "Bulan"
+        '
+        'cmbbln2
+        '
+        Me.cmbbln2.FormattingEnabled = True
+        Me.cmbbln2.Location = New System.Drawing.Point(96, 14)
+        Me.cmbbln2.Name = "cmbbln2"
+        Me.cmbbln2.Size = New System.Drawing.Size(193, 21)
+        Me.cmbbln2.TabIndex = 242
+        '
+        'totalbalance
+        '
+        Me.totalbalance.Location = New System.Drawing.Point(525, 370)
+        Me.totalbalance.Multiline = True
+        Me.totalbalance.Name = "totalbalance"
+        Me.totalbalance.ReadOnly = True
+        Me.totalbalance.Size = New System.Drawing.Size(172, 21)
+        Me.totalbalance.TabIndex = 241
+        '
+        'txttotal
+        '
+        Me.txttotal.Location = New System.Drawing.Point(525, 343)
+        Me.txttotal.Multiline = True
+        Me.txttotal.Name = "txttotal"
+        Me.txttotal.ReadOnly = True
+        Me.txttotal.Size = New System.Drawing.Size(172, 21)
+        Me.txttotal.TabIndex = 239
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(438, 347)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(31, 13)
+        Me.Label7.TabIndex = 240
+        Me.Label7.Text = "Total"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(438, 376)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(46, 13)
+        Me.Label8.TabIndex = 238
+        Me.Label8.Text = "Balance"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(531, 409)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 237
+        Me.Button2.Text = "&Delete"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(583, 444)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "E&xit"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(488, 444)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "&Simpan"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(188, 411)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(190, 316)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(224, 17)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(103, 320)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 13)
+        Me.Label1.TabIndex = 233
+        Me.Label1.Text = "No Bukti Kas"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(147, 391)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(21, 13)
+        Me.Label10.TabIndex = 231
+        Me.Label10.Text = "GL"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Enabled = False
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(188, 385)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(193, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'txtkodegl
+        '
+        Me.txtkodegl.Location = New System.Drawing.Point(189, 361)
+        Me.txtkodegl.Multiline = True
+        Me.txtkodegl.Name = "txtkodegl"
+        Me.txtkodegl.Size = New System.Drawing.Size(151, 17)
+        Me.txtkodegl.TabIndex = 2
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(128, 368)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(49, 13)
+        Me.Label9.TabIndex = 230
+        Me.Label9.Text = "Kode GL"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(71, 419)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(95, 13)
+        Me.Label6.TabIndex = 229
+        Me.Label6.Text = "Tanggal Transaksi"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(67, 320)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 13)
+        Me.Label5.TabIndex = 228
+        '
+        'txtjml
+        '
+        Me.txtjml.Location = New System.Drawing.Point(189, 465)
+        Me.txtjml.Name = "txtjml"
+        Me.txtjml.Size = New System.Drawing.Size(163, 20)
+        Me.txtjml.TabIndex = 5
+        Me.txtjml.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(83, 468)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(89, 13)
+        Me.Label4.TabIndex = 227
+        Me.Label4.Text = "Jumlah Transaksi"
+        '
+        'cmbjenis
+        '
+        Me.cmbjenis.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest
+        Me.cmbjenis.FormattingEnabled = True
+        Me.cmbjenis.Items.AddRange(New Object() {"Debit", "Kredit"})
+        Me.cmbjenis.Location = New System.Drawing.Point(189, 438)
+        Me.cmbjenis.Name = "cmbjenis"
+        Me.cmbjenis.Size = New System.Drawing.Size(121, 21)
+        Me.cmbjenis.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(93, 441)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
+        Me.Label3.TabIndex = 226
+        Me.Label3.Text = "Jenis Transaksi"
+        '
+        'txtket
+        '
+        Me.txtket.Location = New System.Drawing.Point(190, 339)
+        Me.txtket.Multiline = True
+        Me.txtket.Name = "txtket"
+        Me.txtket.Size = New System.Drawing.Size(224, 17)
+        Me.txtket.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(66, 343)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(111, 13)
+        Me.Label2.TabIndex = 225
+        Me.Label2.Text = "Keterangan Transaksi"
+        '
+        'datagridstok
+        '
+        Me.datagridstok.AllowUserToAddRows = False
+        Me.datagridstok.AllowUserToDeleteRows = False
+        Me.datagridstok.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.datagridstok.Location = New System.Drawing.Point(30, 68)
+        Me.datagridstok.Name = "datagridstok"
+        Me.datagridstok.ReadOnly = True
+        Me.datagridstok.Size = New System.Drawing.Size(664, 240)
+        Me.datagridstok.TabIndex = 219
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(470, 316)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(224, 17)
+        Me.TextBox4.TabIndex = 248
+        Me.TextBox4.Visible = False
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(141, 41)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(224, 17)
+        Me.TextBox5.TabIndex = 249
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(54, 45)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(69, 13)
+        Me.Label12.TabIndex = 250
+        Me.Label12.Text = "No Bukti Kas"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(371, 39)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 251
+        Me.Button3.Text = "&Sort"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'biayalain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(743, 500)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.cmbthn2)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.cmbbln2)
+        Me.Controls.Add(Me.totalbalance)
+        Me.Controls.Add(Me.txttotal)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.txtkodegl)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtjml)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cmbjenis)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtket)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.datagridstok)
+        Me.Name = "biayalain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Biaya Lain"
+        CType(Me.datagridstok, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents cmbthn2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents cmbbln2 As System.Windows.Forms.ComboBox
+    Friend WithEvents totalbalance As System.Windows.Forms.TextBox
+    Friend WithEvents txttotal As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents txtkodegl As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtjml As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents cmbjenis As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtket As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents datagridstok As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+End Class
